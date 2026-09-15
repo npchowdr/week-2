50975420f0d2644939789ba574ff1f2515cf2e86
npchowdr <npchowdr@ucsd.edu>
2026-09-14 20:29:58 -0400

    added functions to apputil and tested them in notebook

--- apputil.py
+++ apputil.py
@@ -4,10 +4,19 @@ import numpy as np
 # update/add code below ...
 
 def ways(n):
-    return None
+    #find the maximum number of nickels that can be used
+    max_nickels = n // 5
+    total_ways = max_nickels + 1 # include the case where no nickels are used
+
+    return total_ways
 
 def lowest_score(names, scores):
-    return None
+    # find index of lowest score using argmin and return corresponding name
+    lowest_score_index = np.argmin(scores)
+    return names[lowest_score_index]
 
 def sort_names(names, scores):
-    return None
+    # sort names based on scores using argsort
+    sorted_indices = np.argsort(scores)
+    sorted_names = [names[i] for i in sorted_indices]
+    return sorted_names
